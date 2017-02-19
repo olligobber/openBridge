9853b9f387d996e05a44955e9c6ec0c10725dfc8
olligobber <sirolligobbervii@yahoo.com.au>
2017-02-19 23:35:35 +1100

    Commented all files

--- js/hand.js
+++ js/hand.js
@@ -14,8 +14,29 @@
 	limitations under the License.
 */
 
-var hand_js_version = "1.1.1"
+/*
+	This project provides a scorepad for rubber bridge, intended to be used on
+	any device, and is currently hosted at
 
+		https://github.com/olligobber/openBridge
+
+	Go to the github page to report any bugs and contribute to the project.
+
+	This script creates a scaffold for scoring individual hands in rubber
+	bridge, and is intended to be as versatile as possible for other apps, not
+	just the latest version of the openBridge scorepad.
+*/
+
+var hand_js_version = "1.1.1" // To avoid cache issues, this can be checked by other files that use hand.js
+
+
+/*
+	Contains five objects for each biddable suit, so they can be compared to
+	each other, and scored. The value for each level bid and each overtrick
+	is stored in the "value" property of each suit, and the bonus for the first
+	level bid, relevant only to No Trumps, is stored in the "first" property of
+	each suit.
+*/
 var suit = {
 
 	Clubs : {
@@ -40,38 +61,63 @@ var suit = {
 
 	NoTrumps : {
 		value : 30,
-		first : 10
+		first : 10 // Since the first level bid is worth 40 and subsequent levels only 30
 	}
 
 };
 
+/*
+	Constructs an object for storing and scoring a hand in rubber bridge.
+
+	Intended use is using the following code:
+
+		var myHand = new Hand();
+*/
 function Hand() {
 	// Default Values
-	this.suit = null; // Suit bid [Clubs, Diamonds, Hearts, Spades, NoTrumps]
+	this.suit = null; // Suit bid (Any suit object)
 	this.level = null; // Level bid [1..7]
-	this.vulnerable = false; // Vulnerability of declarer [true, false]
-	this.result = null; // Tricks won by declarer [0..13], null means result was got exactly
-	this.honours = 0; // Honours held out of 5 for trump suit, or all aces held [-5, -4, 0, 4, 5] Negative indicates opposition holding honours
+	this.vulnerable = false; // Vulnerability of declarer (boolean)
+	this.result = null; // Tricks won by declarer [0..13], null means result was got exactly, with no undertricks or overtricks
+	this.honours = 0; // Honours held, out of 5 in the trump suit, or aces held in no trumps [-5, -4, 0, 4, 5], negative indicates opposition holding honours
 	this.victory = true; // Whether or not the contract was made
 	this.scoreBelow = null; // Score below the line for declarer [0..880]
 	this.scoreAbove = null; // Score above the line for declarer [0..4150]
 	this.scoreOpponent = null; // Score above the line for opposition [0..7750]
 	this.double = 1; // The multiplier for this hand [1, 2, 4]
 
-	this.score = function() { // Score the hand based on set variables, return true if successful
-		if (this.suit == null || this.level == null) { // When hand entry is invalid
+	/*
+		Updates the score values.
+		Returns false if hand is not valid and cannot be scored.
+		Returns true if hand is valid and was successfully scored.
+
+		Does not score rubber bonuses.
+	*/
+	this.score = function() {
+		// Check if hand is valid and can be scored
+		if (this.suit == null || this.level == null) {
 			this.scoreBelow = null;
 			this.scoreAbove = null;
 			this.scoreOpponent = null;
 			return false;
 		}
+
+		// Reset scores
 		this.scoreAbove = 0;
 		this.scoreBelow = 0;
 		this.scoreOpponent = 0;
+
+		// Check if contract was made
 		this.victory = (this.result == null || this.result > this.level + 5);
-		if (this.victory) { // When hand was won
-			this.scoreBelow = (this.suit.value * this.level + this.suit.first) * this.double; // Score below line
-			if (this.result > this.level + 6) { // Score overtricks
+
+		// If contract was made
+		if (this.victory) {
+
+			// Score below the line
+			this.scoreBelow = (this.suit.value * this.level + this.suit.first) * this.double;
+
+			// Score overtricks
+			if (this.result > this.level + 6) {
 				if (this.double == 1) {
 					this.scoreAbove = this.suit.value * (this.result - 6 - this.level);
 				}
@@ -79,33 +125,55 @@ function Hand() {
 					this.scoreAbove = (this.vulnerable + 1) * 50 * this.double * (this.result - 6 - this.level);
 				}
 			}
-			if (this.double != 1) { // Insult bonus
+
+			// Insult bonus
+			if (this.double != 1) {
 				this.scoreAbove += 25 * this.double;
 			}
-			if (this.level > 5) { // Slams
+
+			// Slam bonuses
+			if (this.level > 5) {
 				this.scoreAbove += (500 + this.vulnerable * 250) * (this.level - 5);
 			}
 		}
-		else { // When hand was lost
+
+		// When contract was not made
+		else {
+
+			// Base score for undertricks
 			this.scoreOpponent = 50 * (this.level - this.result + 6) * (this.vulnerable + 1) * this.double;
+
+			// 2nd and subsequent undertricks in doubled or redoubled contracts
 			if (this.level - this.result > -5 && this.double != 1) {
 				this.scoreOpponent += (50 * (this.level - this.result + 5) * this.double);
+
+				// 4th and subsequent undertricks in doubled or redoubled contracts when not vulnerable
 				if (this.level - this.result > -3 && !this.vulnerable) {
 					this.scoreOpponent += (50 * (this.level - this.result + 3) * this.double);
 				}
 			}
 		}
-		if (this.honours != 0) { // Honours bonuses
+
+		// Honours bonuses
+		if (this.honours != 0) {
+
+			// Set honours bonus to be [-5, 0, 5] when in No Trumps
 			if (this.suit == suit.NoTrumps && (this.honours == 4 || this.honours == -4)) {
 				this.honours = this.honours / 4 * 5;
 			}
-			if (this.honours > 0) { // Declarer has honours
+
+			// Declarer has honours
+			if (this.honours > 0) {
 				this.scoreAbove += 50 * (this.honours - 2);
 			}
-			else { // Opposition has honours
+
+			// Opposition has honours
+			else {
 				this.scoreOpponent += 50 * (-this.honours - 2);
 			}
 		}
+
+		// Hand has been validated and scored
 		return true;
 	}
 }
--- js/main.js
+++ js/main.js
@@ -14,16 +14,35 @@
 	limitations under the License.
 */
 
-var main_js_version = "1.0.3"
+/*
+	This project provides a scorepad for rubber bridge, intended to be used on
+	any device, and is currently hosted at
 
+		https://github.com/olligobber/openBridge
+
+	Go to the github page to report any bugs and contribute to the project.
+
+	This script provides a scorepad object which interacts with the buttons
+	and elements in index.html, and relies heavily on hand.js for hand
+	management and scoring.
+*/
+
+var main_js_version = "1.0.3" // To avoid cache issues, this can be checked by other files that use main.js
+
+// Check that hand.js was loaded
 if (typeof hand_js_version == "undefined") {
 	console.error("main.js detected that hand.js was not loaded");
 }
 
+// Check that the most recent version of hand.js was loaded
 if (hand_js_version != "1.1.1") {
 	console.error("main.js detected that the current version of hand.js was not correctly loaded");
 }
 
+/*
+	Add some extra properties to the suits in hand.js for each suit's symbol
+	and their button in the hand editor
+*/
 suit.Clubs.symbol = '&clubs;';
 suit.Clubs.element = document.getElementById('suit').children[0].children[0];
 suit.Diamonds.symbol = '&diams;';
@@ -35,6 +54,11 @@ suit.Spades.element = document.getElementById('suit').children[3].children[0];
 suit.NoTrumps.symbol = '\\';
 suit.NoTrumps.element = document.getElementById('suit').children[4].children[0];
 
+/*
+	Contains 4 objects for each of the players. Each contains the html element
+	for their button in the hand editor, their symbol for the hand name, their
+	team and their opponent's team.
+*/
 var players = {
 
 	North : {
@@ -67,31 +91,43 @@ var players = {
 
 };
 
+// Elements of index.html for use throughout the rest of the script
 var elements = {
 
+	// The AP button in the hand editor
 	allPass : document.getElementById('declarer').children[4].children[0],
 
+	// A list of buttons for the level bid in the hand editor
 	level : [null],
 
+	// Result text in the hand editor
 	result : document.getElementById('result'),
 
+	// Entire hand editor area, so it can be shown and hidden as appropriate
 	buttons : document.getElementById('buttons'),
 
+	// Dropdown for selecting the hand multiplier
 	double : document.getElementById('double'),
 
+	// Dropdown for selecting honours bonuses
 	honours : document.getElementById('honours'),
 
+	// The tbody element that contains the hands in the scorepad
 	history : document.getElementById('history').children[0].children[0],
 
+	// Where points above the line are scored
 	above : {
 		we : document.getElementById("we_above"),
 		they : document.getElementById("they_above")
 	},
 
+	// Scores below the line
 	scores : document.getElementById("scorepad").children[0].children[1],
 
+	// The element below the scores below the line
 	botpad : document.getElementById("botpad"),
 
+	// Where the totals for the scorepad are shown
 	total : {
 		we : document.getElementById("total").children[0],
 		they : document.getElementById("total").children[1]
@@ -99,10 +135,12 @@ var elements = {
 
 };
 
+// Adds the buttons for levels bid to the list elements.level
 for (var i=0; i<7; ++i) {
 	elements.level.push(document.getElementById('level').children[i].children[0]);
 }
 
+// Returns a string representing the number of undertricks/overtricks in a hand
 function renderResult(level, result) {
 	if (result == null) {
 		return "+0";
@@ -113,7 +151,12 @@ function renderResult(level, result) {
 	return "" + (result - 6 - level);
 }
 
-function elt(type) { // Borrowed from http://eloquentjavascript.net/13_dom.html
+/*
+	Creates a html element of a given type that contains given elements
+
+	Borrowed from http://eloquentjavascript.net/13_dom.html
+*/
+function elt(type) {
 	var node = document.createElement(type);
 	for (var i = 1; i < arguments.length; i++) {
 		var child = arguments[i];
@@ -125,6 +168,7 @@ function elt(type) { // Borrowed from http://eloquentjavascript.net/13_dom.html
 	return node;
 }
 
+// Returns a string that describes the hand bid and its result
 Hand.prototype.render = function() {
 	if (this.allPass) {
 		return "AP";
@@ -151,34 +195,46 @@ Hand.prototype.render = function() {
 	}
 }
 
-Hand.prototype.index = null;
-Hand.prototype.element = null;
-Hand.prototype.declarer = null;
-Hand.prototype.allPass = false;
+// Some added default values for hands
+Hand.prototype.index = null; // The index of this hand (or its most recently submitted version) in scorepad.allHands
+Hand.prototype.element = null; // The element for this hand on index.html
+Hand.prototype.declarer = null; // The hand's declarer (Any player object)
+Hand.prototype.allPass = false; // Whether all players passed or not
 
+/*
+	The scorepad element stores the variables associated with this scorepad,
+	and the functions necessary to update it, accessed by buttons in index.html
+*/
 var scorepad = {
-	currentHand : null,
-	allHands : [],
-	handCount : 0,
-	scoreElements : [],
+	currentHand : null, // The hand currently being edited
+	allHands : [], // All hands submitted to the scorepad
+	handCount : 0, // The number of non-null elements in allHands
+	scoreElements : [], // The elements of scores below the line
 
+	// Clears out any hand currently being edited, after or without submitting it
 	clearHand : function() {
-		elements.buttons.style.display = 'none';
+		elements.buttons.style.display = 'none'; // Hide the hand editor
 
+		// If no hand was being edited
 		if (this.currentHand == null) {
 			return;
 		}
 
+		// If the hand being edited had never been submitted, delete its element
 		if (this.currentHand.index == null) {
 			this.currentHand.element.outerHTML = "";
 		}
+		// If the hand being edited had been submitted previously, change its element's appearance
 		else {
 			this.currentHand.element.className = "";
 		}
 
+		// Update the all pass button
 		if (this.currentHand.allPass) {
 			elements.allPass.className = "";
 		}
+
+		// Update the level, suit and declarer buttons
 		else {
 			if (this.currentHand.level != null) {
 				elements.level[this.currentHand.level].className = "";
@@ -191,19 +247,26 @@ var scorepad = {
 			}
 		}
 
-		elements.double.value = 1;
-		elements.honours.value = 0;
-		elements.result.innerHTML = "";
-		this.currentHand = null;
+		elements.double.value = 1; // Reset the double dropdown
+		elements.honours.value = 0; // Reset the honours dropdown
+		elements.result.innerHTML = ""; // Clear the hand result
+		this.currentHand = null; // Delete the unsubmitted hand
 	},
 
+	// Update the result element
 	updateResult : function() {
+
+		// If no hand is currently being edited
 		if (this.currentHand == null) {
 			elements.result.innerHTML = "";
 		}
+
+		// If the hand otherwise does not need a result
 		else if (this.currentHand.allPass || this.currentHand.level == null) {
 			elements.result.innerHTML = "";
 		}
+
+		// Render the result with appropriate spacing
 		else if (this.currentHand.result == null) {
 			if (this.currentHand.level < 4) {
 				elements.result.innerHTML = "&nbsp;" + (this.currentHand.level+6) + "&nbsp;" + renderResult(this.currentHand.level, this.currentHand.result);
@@ -222,23 +285,29 @@ var scorepad = {
 		}
 	},
 
+	// Update the scorepad's scores
 	updateScores : function() {
+
+		// Clear out all scores in the scorepad
 		elements.above.we.innerHTML = "";
 		elements.above.they.innerHTML = "";
 		while (this.scoreElements.length > 0) {
 			this.scoreElements.pop().outerHTML = "";
 		}
 
+		// Total score by each team
 		var totals = {
 			we : 0,
 			they : 0
 		};
 
+		// Total score below the line for each team
 		var below = {
 			we : 0,
 			they : 0
 		};
 
+		// Number of games scored by each team in the current rubber
 		var games = {
 			we : 0,
 			they : 0
@@ -251,90 +320,126 @@ var scorepad = {
 			if (this.allHands[i].allPass) {
 				continue;
 			}
+
+			// Apply vulnerability to the hand and update its score
 			this.allHands[i].vulnerable = (games[this.allHands[i].declarer.team] == 1);
 			this.allHands[i].score();
 
+			// Place score below the line
 			if (this.allHands[i].scoreBelow != 0) {
 				totals[this.allHands[i].declarer.team] += this.allHands[i].scoreBelow;
+
+				// Add a score below the line in the left column
 				if (this.allHands[i].declarer.team == "we") {
 					elements.scores.insertBefore(elt("tr", elt("td", "" + this.allHands[i].scoreBelow), elt("td")), elements.botpad);
 				}
+
+				// Add a score below the line in the right column
 				else {
 					elements.scores.insertBefore(elt("tr", elt("td"), elt("td", "" + this.allHands[i].scoreBelow)), elements.botpad);
 				}
-				this.scoreElements.push(elements.botpad.previousSibling);
+
+				this.scoreElements.push(elements.botpad.previousSibling); // Add the new score element to the list
+
 				below[this.allHands[i].declarer.team] += this.allHands[i].scoreBelow;
+
+				// A game has been achieved
 				if (below[this.allHands[i].declarer.team] > 99) {
-					elements.botpad.previousSibling.className = "game";
+					elements.botpad.previousSibling.className = "game"; // Add a line below this score
 					games[this.allHands[i].declarer.team]++;
 					below.we = 0;
 					below.they = 0;
 				}
 			}
 
+			// Place a score above the line for declarer
 			if (this.allHands[i].scoreAbove != 0) {
 				totals[this.allHands[i].declarer.team] += this.allHands[i].scoreAbove;
 				elements.above[this.allHands[i].declarer.team].innerHTML = this.allHands[i].scoreAbove + "<br>" + elements.above[this.allHands[i].declarer.team].innerHTML;
 			}
 
+			// Place a score above the line for opposition
 			if (this.allHands[i].scoreOpponent != 0) {
 				totals[this.allHands[i].declarer.opponent] += this.allHands[i].scoreOpponent;
 				elements.above[this.allHands[i].declarer.opponent].innerHTML = this.allHands[i].scoreOpponent + "<br>" + elements.above[this.allHands[i].declarer.opponent].innerHTML;
 			}
 
+			// Add a rubber bonus above the line
 			if (games[this.allHands[i].declarer.team] == 2) {
+
+				// Rubber in 3
 				if (games[this.allHands[i].declarer.opponent] == 1) {
 					totals[this.allHands[i].declarer.team] += 500;
 					elements.above[this.allHands[i].declarer.team].innerHTML = "500<br>" + elements.above[this.allHands[i].declarer.team].innerHTML;
 				}
+
+				// Rubber in 2
 				else {
 					totals[this.allHands[i].declarer.team] += 700;
 					elements.above[this.allHands[i].declarer.team].innerHTML = "700<br>" + elements.above[this.allHands[i].declarer.team].innerHTML;
 				}
+
 				games.we = 0;
 				games.they = 0;
 			}
 
 		}
 
+		// Update totals
 		elements.total.we.innerHTML = totals.we;
 		elements.total.they.innerHTML = totals.they;
 
 	},
 
+	// Add a new hand and open the hand editor
 	newHand : function() {
 		this.clearHand();
 		this.currentHand = new Hand();
-		elements.history.insertBefore(elt("tr", elt("td"), elt("td", elt("button", "Edit")), elt("td", elt("button", "Delete"))), elements.history.children[1]);
+		elements.history.insertBefore(elt("tr", elt("td"), elt("td", elt("button", "Edit")), elt("td", elt("button", "Delete"))), elements.history.children[1]); // Insert hand's element
 		this.currentHand.element = elements.history.children[1];
 		this.currentHand.element.className = 'selected';
 		this.currentHand.element.getElementsByTagName('button')[0].setAttribute('type', 'button');
 		this.currentHand.element.getElementsByTagName('button')[1].setAttribute('type', 'button');
-		elements.buttons.style.display = null;
+		elements.buttons.style.display = null; // Show hand editor
 	},
 
+	// Delete a hand from the scorepad
 	removeHand : function(index) {
-		this.allHands[index].element.outerHTML = "";
-		this.allHands[index] = null;
+		this.allHands[index].element.outerHTML = ""; // Remove the element
+		this.allHands[index] = null; // Remove the object
 		this.updateScores();
 		this.handCount--;
+
+		// Clear the list if there are no hands left
 		if (this.handCount == 0) {
 			this.allHands = [];
 		}
 	},
 
+	// Submit the hand currently being edited and close the hand editor
 	submitHand : function() {
+
+		// Check the hand is valid to be scored
 		if (this.currentHand.allPass || this.currentHand.score()) {
+
+			// Update values from dropdowns
 			this.currentHand.double = parseInt(elements.double.value);
 			this.currentHand.honours = parseInt(elements.honours.value);
+
+			// Honours cannot be recorded properly with no declarer
 			if (this.currentHand.allPass && this.currentHand.declarer == null) {
-				this.currentHand.honours = 0; // Honours cannot be recorded properly with no declarer
+				this.currentHand.honours = 0;
 			}
+
+			// Negatives are reversed when "they" are declarer
 			else if (this.currentHand.declarer.team == "they") {
 				this.currentHand.honours = - this.currentHand.honours;
 			}
+
+			// Update hand's description
 			this.currentHand.element.children[0].innerHTML = this.currentHand.render();
 
+			// If this hand has never been submitted before, add it to the list and update its buttons' commands
 			if (this.currentHand.index == null) {
 				this.currentHand.index = this.allHands.length;
 				this.allHands.push(this.currentHand);
@@ -351,40 +456,56 @@ var scorepad = {
 
 	},
 
+	// Load a previously submitted hand and open the hand editor
 	editHand : function(index) {
 		this.clearHand();
 		this.currentHand = Object.create(this.allHands[index]);
 		this.currentHand.element.className = 'selected';
 
+		// Select all pass button
 		if (this.currentHand.allPass) {
 			elements.allPass.className = 'selected';
 		}
+
+		// Select buttons for declarer, level and suit
 		else {
 			this.currentHand.declarer.element.className = 'selected';
 			elements.level[this.currentHand.level].className = 'selected';
 			this.currentHand.suit.element.className = 'selected';
 		}
 
+		// Update doubles dropdown
 		elements.double.value = this.currentHand.double;
+
+		// A hand with no declarer cannot have an honours bonus
 		if (this.currentHand.allPass && this.currentHand.declarer == null) {
 			elements.honours.value = 0;
 		}
+
+		// Update the honours dropdown
 		else if (this.currentHand.declarer.team == 'we') {
 			elements.honours.value = this.currentHand.honours;
 		}
+
+		// Update the honours dropdown, with negatives swapped if "they" are declarer
 		else {
 			elements.honours.value = - this.currentHand.honours;
 		}
 
 		this.updateResult();
 
-		elements.buttons.style.display = null;
+		elements.buttons.style.display = null; // Show the hand editor buttons
 	},
 
+	// Set the all pass flag to true and update button selections accordingly
 	allPassOn : function() {
+
+		// If hand wasn't already an all pass hand
 		if (!this.currentHand.allPass) {
 			this.currentHand.allPass = true;
-			elements.allPass.className = 'selected';
+			elements.allPass.className = 'selected'; // Select the all pass button
+
+			// Deselect other buttons
 			if (this.currentHand.declarer != null) {
 				this.currentHand.declarer.element.className = '';
 			}
@@ -394,14 +515,20 @@ var scorepad = {
 			if (this.currentHand.suit != null) {
 				this.currentHand.suit.element.className = '';
 			}
+
 			this.updateResult();
 		}
 	},
 
+	// Set the all pass flag to false and update button selections accordingly
 	allPassOff : function() {
+
+		// If the hand was flagged as all pass
 		if (this.currentHand.allPass) {
 			this.currentHand.allPass = false;
-			elements.allPass.className = '';
+			elements.allPass.className = ''; // Deslect the all psss button
+
+			// Select buttons that were selected before all pass was turned on
 			if (this.currentHand.declarer != null) {
 				this.currentHand.declarer.element.className = 'selected';
 			}
@@ -411,45 +538,67 @@ var scorepad = {
 			if (this.currentHand.suit != null) {
 				this.currentHand.suit.element.className = 'selected';
 			}
+
 			this.updateResult();
 		}
 	},
 
+	// Set the declarer for this hand and update button selections accordingly
 	setDeclarer : function(newDeclarer) {
-		this.allPassOff();
+
+		this.allPassOff(); // An all pass hand cannot have a declarer, so turn off all pass
+
+		// Deselect the previous declarer
 		if (this.currentHand.declarer != null) {
 			this.currentHand.declarer.element.className = '';
 		}
+
 		this.currentHand.declarer = newDeclarer;
-		newDeclarer.element.className = 'selected';
+		newDeclarer.element.className = 'selected'; // Select the button for this declarer
 	},
 
+	// Set the suit for this hand and update button selections accordingly
 	setSuit : function(newSuit) {
-		this.allPassOff();
+
+		this.allPassOff(); // An all pass hand cannot have a suit, so turn off all pass
+
+		// Deselect the previous suit
 		if (this.currentHand.suit != null) {
 			this.currentHand.suit.element.className = '';
 		}
+
 		this.currentHand.suit = newSuit;
-		newSuit.element.className = 'selected';
+		newSuit.element.className = 'selected'; // Select the button for this suit
 	},
 
+	// Increment or decrement the number of tricks won
 	changeResult : function(change) {
+
+		// If the hand was previously marked as won exactly
 		if (this.currentHand.result == null) {
+
+			// Hands where a result is not relevant
 			if (this.currentHand.level == null || this.currentHand.allPass) {
 				return;
 			}
+
+			// The result cannot be incremented
 			if (this.currentHand.level == 7 && change > 0) {
 				return;
 			}
+
+			// Set the number of tricks won to an integer if won exactly
 			this.currentHand.result = this.currentHand.level + 6;
 		}
 
 		this.currentHand.result += change;
 
+		// Minimum bound for result
 		if (this.currentHand.result < 0) {
 			this.currentHand.result = 0;
 		}
 
+		// Maximum bound for result
 		if (this.currentHand.result > 13) {
 			this.currentHand.result = 13;
 		}
@@ -458,17 +607,25 @@ var scorepad = {
 
 	},
 
+	// Set the level bid for this hand and update button selections accordingly
 	setLevel : function(newLevel) {
-		this.allPassOff();
+
+		this.allPassOff(); // An all pass hand cannot have a level, so turn off all pass
+
+		// If a level was previously selected
 		if (this.currentHand.level != null) {
-			elements.level[this.currentHand.level].className = '';
+			elements.level[this.currentHand.level].className = ''; // Deselect the previous level
+
+			// Update the result if neccessary
 			if (this.currentHand.result != null) {
 				this.changeResult(newLevel - this.currentHand.level);
 			}
+
 		}
+
 		this.currentHand.level = newLevel;
 		this.updateResult();
-		elements.level[newLevel].className = 'selected';
+		elements.level[newLevel].className = 'selected'; // Select the button for this level
 	}
 
 };
